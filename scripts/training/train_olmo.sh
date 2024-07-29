99bf7d59e50355d206683c7f728c6054b97ee5da
Ryan Wang <ryanyxw@gmail.com>
2024-07-29 12:18:04 -0700

    added custom embedding layer to intervene on hidden state for olmo training code. Begin editing custom conversion script and transformer model with new added embedding matrix

--- scripts/training/train_olmo.sh
+++ scripts/training/train_olmo.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=3-0:00
 #SBATCH --job-name=sbatch
 #SBATCH --output=slurm_out/out_%j.txt
-#SBATCH --exclude=glamor-ruby,dill-sage
-#SBATCH --gres="gpu:a6000:4"
+#SBATCH --gres="gpu:a6000:1"
+#SBATCH --nodelist=dill-sage
 #SBATCH --ntasks=16
 
 ROOT_DIR=./../..
@@ -12,6 +12,7 @@ MODEL_DIR=${ROOT_DIR}/models
 OLMO_DIR=${ROOT_DIR}/OLMo
 CONFIG_DIR=${ROOT_DIR}/configs
 #SBATCH --nodelist=glamor-ruby
+#SBATCH --exclude=glamor-ruby,dill-sage
 
 
 export CUDA_HOME=/home/ryan/miniconda3/envs/olmo
@@ -22,15 +23,23 @@ config_file="${CONFIG_DIR}/olmo/OLMo-1B_pretrain_scratch.yaml"
 
 PORT=29502
 load_path=""
-data_paths="/mnt/nfs1/ryan/decouple/data/olmo_training/pretrain_from_scratch/0-99-toxic_0-0001-safe/train/filtered/input_ids.npy"
-data_label_mask_paths="/mnt/nfs1/ryan/decouple/data/olmo_training/pretrain_from_scratch/0-99-toxic_0-0001-safe/train/filtered/label_mask.npy"
-save_folder="/home/ryan/decouple/models/olmo_ckpt/prefromscratch/0-99-toxic_0-0001-safe/filtered_exp2"
+data_paths="/mnt/nfs1/ryan/decouple/data/olmo_training/pretrain_from_scratch/0-99-toxic_0-0001-safe/train/orig/input_ids.npy"
+data_label_mask_paths="/mnt/nfs1/ryan/decouple/data/olmo_training/pretrain_from_scratch/0-99-toxic_0-0001-safe/train/orig/label_mask.npy"
+save_folder="/home/ryan/decouple/models/olmo_ckpt/prefromscratch/0-99-toxic_0-0001-safe/activationbias_extreme_exp3"
 
-wandb_name="filtered_exp2"
+wandb_name="activationbias_extreme_exp3"
 wandb_group="OLMO-1B_scratch"
 
+# for determining which loss to use for each label mask
+label_mask_to_loss='{"no_loss": [0], "ce_loss": [1, 2, 3], "unlikelihood": [4], "policy": [4], "cringe": [4]}'
+
+# for adding class bias to the last hidden state
+add_class_bias=True
+num_classes=2
+label_mask_to_class_bias='[0, 0, 1, 1]' # this should have length number of unique label_mask_ids, mapping to numbers in range num_classes
+
 num_steps=2527
-num_gpus=4
+num_gpus=1
 global_train_batch_size=64
 device_train_microbatch_size=16
 
@@ -53,6 +62,10 @@ torchrun --nproc_per_node=${num_gpus} --master_port=${PORT} ${OLMO_DIR}/scripts/
   --wandb.group=${wandb_group}\
   --max_duration=${num_steps}\
   --save_interval_unsharded=${save_interval_unsharded}\
+  --label_mask_to_loss="${label_mask_to_loss}"\
+  --model.add_class_bias=${add_class_bias}\
+  --model.num_classes=${num_classes}\
+  --label_mask_to_class_bias="${label_mask_to_class_bias}"\
 
 #  --eval_interval=${eval_interval}\
 #  --device_eval_batch_size=${device_eval_batch_size}\
